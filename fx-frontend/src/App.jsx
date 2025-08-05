b2c455b4cfe6e59d2a69dec730a772e2529b5c59
Projdct-Web-Shopee-Website <lehoangvi.work@gmail.com>
2025-08-05 11:34:42 +0700

    w

--- fx-frontend/src/App.jsx
+++ fx-frontend/src/App.jsx
@@ -36,6 +36,7 @@ import Profile from './pages/Profile';
 import Terms from './pages/Terms'; 
 import Disclaimer from './pages/Disclamer'; 
 import ThemeToggle from './components/ThemeToggle'; 
+import Analytics from './pages/Analytics';
 
 import './css/theme.css'
 import './App.css';
@@ -949,7 +950,9 @@ function AppContent() {
                       backdropFilter: 'blur(10px)',
                       border: `1px solid ${isDark ? 'rgba(255, 255, 255, 0.2)' : 'rgba(255, 255, 255, 0.3)'}`,
                       transition: 'all 0.3s ease'
-                    }}><ArchiveRateForm /></div>
+                    }}><ArchiveRateForm /></div> 
+
+                    <div><Analytics /></div>
                   </div>
                 </div>
               ) : (
@@ -975,7 +978,7 @@ function AppContent() {
             <Route path="/rates" element={<Rates />} />  
             <Route path="/SaveForm" element={<SaveForm />} /> 
             <Route path="/converter" element={<Converter />} />
-            
+            <Route path="/analytics" element={<Analytics />} />
           </Routes> 
         </main>
 
